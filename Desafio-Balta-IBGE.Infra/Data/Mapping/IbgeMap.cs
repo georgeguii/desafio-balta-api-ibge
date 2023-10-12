731059b6c07572a92e46043fad6b831df3fc0400
George Guilherme <george.gui2000@gmail.com>
2023-10-12 19:45:06 -0300

    [New] Criado Mapping da entidade IBGE

---
+++ Desafio-Balta-IBGE.Infra/Data/Mapping/IbgeMap.cs
@@ -0,0 +1,28 @@
+﻿using Desafio_Balta_IBGE.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Desafio_Balta_IBGE.Infra.Data.Mapping;
+
+public sealed class IbgeMap : IEntityTypeConfiguration<Ibge>
+{
+    public void Configure(EntityTypeBuilder<Ibge> builder)
+    {
+        builder.ToTable("Ibge");
+
+        builder.Ignore(x => x.Id);
+
+        builder.HasKey(x => x.IbgeId);
+
+        builder.Property(x => x.State)
+            .HasColumnName("State")
+            .HasColumnType("char(2)")
+            .IsRequired();
+
+        builder.Property(x => x.City)
+            .HasColumnName("City")
+            .HasColumnType("varchar(50)")
+            .IsRequired();
+
+    }
+}
