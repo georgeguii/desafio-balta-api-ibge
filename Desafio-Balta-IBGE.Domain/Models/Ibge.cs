8aed63bd62d3b6624099679c6ce085af2f9bfa1e
George Guilherme <george.gui2000@gmail.com>
2023-10-12 14:35:34 -0300

    [New] Criada entidade IBGE

---
+++ Desafio-Balta-IBGE.Domain/Models/Ibge.cs
@@ -0,0 +1,20 @@
+﻿using Desafio_Balta_IBGE.Shared.Entities;
+
+namespace Desafio_Balta_IBGE.Domain.Models;
+
+public class Ibge : Entity
+{
+    public int IbgeId { get; set; }
+    public string City { get; set; } = string.Empty;
+    public string State { get; set; } = string.Empty;
+
+    public Ibge()
+    {
+    }
+
+    public Ibge(string city, string state)
+    {
+        City = city;
+        State = state;
+    }
+}
