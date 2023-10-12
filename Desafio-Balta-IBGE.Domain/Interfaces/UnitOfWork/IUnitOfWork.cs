4752eb2f4e02b81e588b88e6199674bb3818610b
George Guilherme <george.gui2000@gmail.com>
2023-10-12 19:43:48 -0300

    [New] Criadas as interfaces

---
+++ Desafio-Balta-IBGE.Domain/Interfaces/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace Desafio_Balta_IBGE.Domain.Interfaces.UnitOfWork;
+
+public interface IUnitOfWork : IDisposable
+{
+    void BeginTransaction();
+    void Commit();
+    void Rollback();
+}
