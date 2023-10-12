79b4643cbfb9c66a3ff663dcb00c01efcf902b07
George Guilherme <george.gui2000@gmail.com>
2023-10-12 20:33:59 -0300

    [Fix] Modificação de GetById para receber somente um int

--- Desafio-Balta-IBGE.Domain/Interfaces/BaseRepository/IBaseRepository.cs
+++ Desafio-Balta-IBGE.Domain/Interfaces/BaseRepository/IBaseRepository.cs
@@ -4,8 +4,8 @@ namespace Desafio_Balta_IBGE.Domain.Interfaces.BaseRepository;
 
 public interface IBaseRepository<TEntity>
 {
-    Task<IEnumerable<TEntity>> Get();
-    Task<TEntity> GetbyId(Expression<Func<TEntity, bool>> predicate);
+    Task<IEnumerable<TEntity>> GetAsync();
+    ValueTask<TEntity?> GetByIdAsync(int id);
     Task AddAsync(TEntity entity);
     void Update(TEntity entity);
     void DeleteAsync(TEntity entity);
