3b3a141708e0d4b2429e38de16e2fbadb66f8c90
George Guilherme <george.gui2000@gmail.com>
2023-10-12 23:40:28 -0300

    [New] Finaliza configuração para acesso ao banco

---
+++ Desafio-Balta-IBGE.API/Extensions/ServicesExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using Desafio_Balta_IBGE.Infra.Data.Context;
+
+namespace Desafio_Balta_IBGE.API.Extensions;
+
+public static class ServicesExtension
+{
+    public static void AddContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        #region Connection
+        string? connection = configuration.GetConnectionString("Database");
+        services.AddDbContext<IbgeContext>(options => options.UseSqlServer(connection));
+        #endregion
+    }
+}
