7d867a6e8dd363afb22bc280f128bd514fc739a0
guilherme-beltran <gtmbeltran@hotmail.com>
2023-10-17 22:27:39 -0300

    [fix] Removido itens desnecessários

--- Desafio-Balta-IBGE.Shared/Extensions/Criptography.cs
+++ Desafio-Balta-IBGE.Shared/Extensions/Criptography.cs
@@ -10,14 +10,6 @@
             return senhaCriptografada;
         }
 
-        public static string Encrypt2(string password)
-        {
-            var salt = BCrypt.Net.BCrypt.GenerateSalt();
-            var senhaCriptografada = BCrypt.Net.BCrypt.HashPassword(password, salt);
-
-            return senhaCriptografada;
-        }
-
         public static bool CompareHash(string password, string hash)
         {
             return BCrypt.Net.BCrypt.Verify(password, hash);
