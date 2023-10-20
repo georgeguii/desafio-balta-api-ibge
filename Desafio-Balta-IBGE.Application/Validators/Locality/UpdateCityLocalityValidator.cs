7d112665e71bc99b7cfd35af184c1809c64bc524
George Guilherme <george.gui2000@gmail.com>
2023-10-20 17:58:49 -0300

    [Fix] Adiciona validacao de código do IBGE no validator de update

--- Desafio-Balta-IBGE.Application/Validators/Locality/UpdateCityLocalityValidator.cs
+++ Desafio-Balta-IBGE.Application/Validators/Locality/UpdateCityLocalityValidator.cs
@@ -7,6 +7,14 @@ public class UpdateCityLocalityValidator : AbstractValidator<UpdateCityLocalityR
 {
     public UpdateCityLocalityValidator()
     {
+        RuleFor(x => x.IbgeId)
+            .NotEmpty()
+                .WithMessage("O código do IBGE não pode ser vazio.")
+            .Length(7, 7)
+                .WithMessage("O Código do IBGE deve possuir 7 caracteres.")
+            .Matches(@"^[0-9]+$")
+                .WithMessage("O código do IBGE deve possuir somente números.");
+
         RuleFor(l => l.City)
             .NotEmpty()
                 .WithMessage("A cidade não pode ser vazia.")
